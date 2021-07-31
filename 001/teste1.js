32b47c48fb6abd69aec03a2a334d50e06e35cd3f
codinscode <heronnestg@hotmail.com>
2021-07-31 16:26:43 -0300

    ------

---
+++ 001/teste1.js
@@ -0,0 +1,33 @@
+
+var a = 3
+var b = 5
+
+
+function somar(v1, v2) {
+
+    return v1 + v2
+}
+
+console.log(somar(a, b))
+console.log('-----------------')
+
+let x1 = 4
+const x2 = 10
+
+
+console.log(`x1: ${x1}`)
+console.log(`x2: ${x2}`)
+
+x1 += 2
+//x2 += 5  // dá erro
+
+
+console.log(`x1: ${x1}`)
+console.log(`x2: ${x2}`)
+
+
+
+
+
+
+
