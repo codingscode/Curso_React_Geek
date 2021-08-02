086a25481fb95e28265e896b680928b646b6725e
codinscode <heronnestg@hotmail.com>
2021-08-02 10:03:30 -0300

    parte 4

---
+++ 002/react-basico/src/components/Comentario.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './Comentario.css'
+
+const Comentario = () => (
+   <div className="Comentario" >Olá eu sou um componente</div>
+)
+
+export default Comentario
+
+
+
+
+
+
