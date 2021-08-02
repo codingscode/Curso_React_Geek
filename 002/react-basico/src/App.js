c6030933cf6c064778661e29ea8a94d4cdda25e4
codinscode <heronnestg@hotmail.com>
2021-08-02 10:31:58 -0300

    parte 6

--- 002/react-basico/src/App.js
+++ 002/react-basico/src/App.js
@@ -7,7 +7,16 @@ function App() {
    return (
       <div>
         <div>Oi</div>
-        <Comentario />
+        <Comentario nome="Samuel" email="samueldj@gmail.com" data={new Date(2021, 3, 14)} >
+           Olá tudo legal
+        </Comentario>
+        <Comentario nome="Angela" email="angeliscosmeticos@gmail.com" data={new Date(2020, 5, 10)} >
+           Tá de boa
+        </Comentario>
+        <Comentario nome="Michael" email="michel21@gmail.com" data={new Date(2019, 7, 23)} >
+           Beleza
+        </Comentario>
+
       </div> 
    )
 }
--- 002/react-basico/src/components/Comentario.js
+++ 002/react-basico/src/components/Comentario.js
@@ -2,15 +2,14 @@
 import React from 'react'
 import './Comentario.css'
 
-const Comentario = () => {
-   const numero1 = 6
-   const numero2 = 10
-
+const Comentario = (props) => {
+   
    return (
         <div className="Comentario" >
-            <div>Olá eu sou um componente</div>
-            <div>soma 2 + 3: {2 + 3}</div>
-            <div>numero1 + numero2: {numero1 + numero2}</div>
+            <div>{props.nome}</div>
+            <div>{props.email}</div>
+            <div>{props.children}</div>
+            <div>{props.data.toString()}</div>
         </div>
    )
 }
