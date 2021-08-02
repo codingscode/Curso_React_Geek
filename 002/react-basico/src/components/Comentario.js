7773b94c392006697a6c0e7a9e22adb03f255795
codinscode <heronnestg@hotmail.com>
2021-08-02 10:11:53 -0300

    parte 5

--- 002/react-basico/src/components/Comentario.js
+++ 002/react-basico/src/components/Comentario.js
@@ -2,14 +2,17 @@
 import React from 'react'
 import './Comentario.css'
 
-const Comentario = () => (
-   <div className="Comentario" >Olá eu sou um componente</div>
-)
+const Comentario = () => {
+   const numero1 = 6
+   const numero2 = 10
+
+   return (
+        <div className="Comentario" >
+            <div>Olá eu sou um componente</div>
+            <div>soma 2 + 3: {2 + 3}</div>
+            <div>numero1 + numero2: {numero1 + numero2}</div>
+        </div>
+   )
+}
 
 export default Comentario
-
-
-
-
-
-
