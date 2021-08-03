f196dfe6b3e3031887b86b6f59f6ccdaf9cfbf70
codinscode <heronnestg@hotmail.com>
2021-08-03 12:32:27 -0300

    parte 8

--- 002/react-basico/src/App.js
+++ 002/react-basico/src/App.js
@@ -4,12 +4,22 @@ import './App.css'
 
 
 class App extends Component {
+   state = {
+      comentarios: [
+         {nome: 'Samuel', email: 'samueldj@gmail.com', data: new Date(2021, 3, 14), mensagem: 'Olá tudo legal'},
+         {nome: 'Angela', email: 'angeliscosmeticos@gmail.com', data: new Date(2020, 5, 10), mensagem: 'Tá de boa'},
+         {nome: 'Michael', email: 'michel21@gmail.com', data: new Date(2019, 7, 23), mensagem: 'Beleza'}
+      ]
+   }
 
-   
    render() {
       return (
          <div>
            <div>Oi</div>
+           {this.state.comentarios.map(cada => (
+               console.log(cada)
+           ))}
+
            <Comentario nome="Samuel" email="samueldj@gmail.com" data={new Date(2021, 3, 14)} >
               Olá tudo legal
            </Comentario>
