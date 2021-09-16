f65d14f868eca8af28aa1f8db8428183e57644d4
codingscode <heronnestg@hotmail.com>
2021-09-15 21:29:55 -0300

    parte 53

--- 004/react_http/src/App.js
+++ 004/react_http/src/App.js
@@ -6,6 +6,15 @@ import Usuarios from './components/Usuarios/Usuarios'
 function App() {
   return (
     <div className="App">
+      <header>
+         <nav>
+            <ul>
+               <li><a href="/" >Início</a></li>
+               <li><a href="/usuarios" >Usuários Cadastrados</a></li>
+               <li><a href="/adicionar" >Adicionar Usuário</a></li>                              
+            </ul>
+         </nav>
+      </header>
       <main>
         <Usuarios />
       </main>
