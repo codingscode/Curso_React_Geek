e3b11e4d519f04b0bb17839c61fc79c84dbcb498
codingscode <heronnestg@hotmail.com>
2021-10-16 21:52:04 -0300

    parte 68

--- 004/react_http/src/components/DetalhesUsuario/DetalhesUsuario.js
+++ 004/react_http/src/components/DetalhesUsuario/DetalhesUsuario.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 
@@ -6,9 +6,33 @@ function DetalhesUsuario() {
 
    const { id } = useParams()
 
+   const [usuario, setUsuario] = useState({})
+
+   useEffect(() => {
+      fetch(`https://reqres.in/api/users/${id}`)
+         .then(resposta => resposta.json())
+         .then(dados => {
+            if (dados.data) {
+               setUsuario({ id: dados.data.id, nome: dados.data.first_name, sobrenome: dados.data.last_name, email: dados.data.email, foto: dados.data.avatar })
+            }
+         })
+   }, [id])
+
+   if (usuario.nome !== undefined) {
+      return (
+         <>
+            <h1>{usuario.nome} {usuario.sobrenome}</h1>
+            <img src={usuario.foto} alt={usuario.nome} />
+            <p>{usuario.email} </p>
+            <Link to="/usuarios" >Voltar</Link>
+         </>
+      )
+   
+   }
+
    return (
       <>
-         <h1>ID: {id}</h1>
+         <h1>Usuário não encontrado!</h1>
          <Link to="/usuarios" >Voltar</Link>
       </>
    )
