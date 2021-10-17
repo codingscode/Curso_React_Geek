baecf6ccd7e429fbf63f9346fdcb9162617cdcaf
codingscode <heronnestg@hotmail.com>
2021-10-16 21:31:52 -0300

    parte 66

--- 004/react_http/src/components/Usuario/Usuario.js
+++ 004/react_http/src/components/Usuario/Usuario.js
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import './Usuario.css'
+
+
 
 function Usuario(props) {
   return (
@@ -9,6 +11,7 @@ function Usuario(props) {
         <li><strong>ID:</strong> {props.usuario.id}</li>
         <li><strong>Nome:</strong> {props.usuario.nome} {props.usuario.sobrenome}</li>
         <li><strong>Email:</strong> {props.usuario.email}</li>
+        <li><Link to={`/usuarios/${props.usuario.id}`} >Detalhes</Link></li>
       </ul>
       <button onClick={props.removerUsuario}>&times;</button>
     </div>
