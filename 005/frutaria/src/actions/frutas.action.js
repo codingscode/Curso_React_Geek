02b8295aac6140ef79b26a2f869420d93980ba97
codingscode <heronnestg@hotmail.com>
2021-10-19 12:36:30 -0300

    parte 74

---
+++ 005/frutaria/src/actions/frutas.action.js
@@ -0,0 +1,17 @@
+
+
+
+const actions = {
+   adicionar: fruta => ({
+      type: 'ADICIONAR_FRUTA',
+      payload: fruta
+   }),
+   adicionar: fruta => ({
+      type: 'REMOVER_FRUTA',
+      payload: fruta
+   })
+}
+
+export {actions}
+
+
