6146e5c8e46288419c9a5d0487ef94caf845fb3a
codingscode <heronnestg@hotmail.com>
2021-10-23 21:53:49 -0300

    parte 84

--- 006/react-formulario/src/components/AdicionaCliente.jsx
+++ 006/react-formulario/src/components/AdicionaCliente.jsx
@@ -8,22 +8,22 @@ const AdicionaCliente = () => {
       <>
          <h1>Cadastro de Clientes</h1>
    
-         <Formik initialValues={{ nome: '', email: '', nascimento: '' }} onSubmit={(values) => {
+         <Formik initialValues={{ nome: 'Mel', email: 'mel@gmail.com', nascimento: '11/09/2019' }} onSubmit={(values) => {
             alert(JSON.stringify(values))
          }} >
             {(props) => (
                <form onSubmit={props.handleSubmit} noValidate >
                   <div className="form-group">
                      <label htmlFor="nome">Nome</label>
-                     <input type="text" id="nome" name="nome" onChange={props.handleChange} />
+                     <input type="text" id="nome" name="nome" value={props.values.nome} onChange={props.handleChange} />
                   </div>
                   <div className="form-group">
                      <label htmlFor="email">Email</label>
-                     <input type="email" id="email" name="email" onChange={props.handleChange} />
+                     <input type="email" id="email" name="email" value={props.values.email} onChange={props.handleChange} />
                   </div>
                   <div className="form-group">
                      <label htmlFor="date">Data de Nascimento</label>
-                     <input type="date" id="nascimento" name="nascimento" onChange={props.handleChange} />
+                     <input type="date" id="nascimento" name="nascimento" value={props.values.nascimento} onChange={props.handleChange} />
                   </div>
                   <button type="submit">Adicionar</button>
                </form>
