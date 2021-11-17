39707fa7062deae17ebce66d3cd992f22f59adc0
codingscode <heronnestg@hotmail.com>
2021-11-16 22:34:35 -0300

    parte 88

--- 006/react-formulario/src/components/AdicionaCliente.jsx
+++ 006/react-formulario/src/components/AdicionaCliente.jsx
@@ -15,6 +15,8 @@ const AdicionaCliente = () => {
             }
             if (!values.email) {
                errors.email = 'O email é obrigatório'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+               errors.email = 'O email é inválido'               
             }
             if (!values.nascimento) {
                errors.nascimento = 'A data de nascimento é obrigatória'
@@ -27,17 +29,17 @@ const AdicionaCliente = () => {
                <form onSubmit={props.handleSubmit} noValidate >
                   <div className="form-group">
                      <label htmlFor="nome">Nome</label>
-                     <input type="text" id="nome" name="nome" value={props.values.nome} onChange={props.handleChange} onBlur={props.handleBlur} />
+                     <input type="text" id="nome" name="nome" value={props.values.nome} onChange={props.handleChange} onBlur={props.handleBlur} className={props.errors.nome && props.touched.nome ? 'eh-invalido' : '' } />
                      {props.errors.nome &&props.touched.nome ? (<div className="feedback-invalido" >{props.errors.nome}</div>) : null}
                   </div>
                   <div className="form-group">
                      <label htmlFor="email">Email</label>
-                     <input type="email" id="email" name="email" value={props.values.email} onChange={props.handleChange} onBlur={props.handleBlur} />
+                     <input type="email" id="email" name="email" value={props.values.email} onChange={props.handleChange} onBlur={props.handleBlur} className={props.errors.email && props.touched.email ? 'eh-invalido' : '' } />
                      {props.errors.email &&props.touched.email ? (<div className="feedback-invalido" >{props.errors.email}</div>) : null}
                   </div>
                   <div className="form-group">
                      <label htmlFor="date">Data de Nascimento</label>
-                     <input type="date" id="nascimento" name="nascimento" value={props.values.nascimento} onChange={props.handleChange} onBlur={props.handleBlur} />
+                     <input type="date" id="nascimento" name="nascimento" value={props.values.nascimento} onChange={props.handleChange} onBlur={props.handleBlur} className={props.errors.nascimento && props.touched.nascimento ? 'eh-invalido' : '' } />
                      {props.errors.nascimento &&props.touched.nascimento ? (<div className="feedback-invalido" >{props.errors.nascimento}</div>) : null}
                   </div>
                   <button type="submit">Adicionar</button>
