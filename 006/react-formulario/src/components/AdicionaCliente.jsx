3ee4a5388c6b980d7f1b8a34da6f72d79b674442
codingscode <heronnestg@hotmail.com>
2021-11-16 21:55:37 -0300

    parte 85

--- 006/react-formulario/src/components/AdicionaCliente.jsx
+++ 006/react-formulario/src/components/AdicionaCliente.jsx
@@ -8,7 +8,19 @@ const AdicionaCliente = () => {
       <>
          <h1>Cadastro de Clientes</h1>
    
-         <Formik initialValues={{ nome: 'Mel', email: 'mel@gmail.com', nascimento: '11/09/2019' }} onSubmit={(values) => {
+         <Formik initialValues={{ nome: '', email: '', nascimento: '' }} validate={(values) => {
+            const errors = {}
+            if (!values.nome) {
+               errors.nome = 'O nome é obrigatório'
+            }
+            if (!values.email) {
+               errors.email = 'O email é obrigatório'
+            }
+            if (!values.nascimento) {
+               errors.nascimento = 'A data de nascimento é obrigatória'
+            }
+            return errors
+         }} onSubmit={(values) => {
             alert(JSON.stringify(values))
          }} >
             {(props) => (
@@ -16,14 +28,17 @@ const AdicionaCliente = () => {
                   <div className="form-group">
                      <label htmlFor="nome">Nome</label>
                      <input type="text" id="nome" name="nome" value={props.values.nome} onChange={props.handleChange} />
+                     {props.errors.nome ? (<div>{props.errors.nome}</div>) : null}
                   </div>
                   <div className="form-group">
                      <label htmlFor="email">Email</label>
                      <input type="email" id="email" name="email" value={props.values.email} onChange={props.handleChange} />
+                     {props.errors.email ? (<div>{props.errors.email}</div>) : null}
                   </div>
                   <div className="form-group">
                      <label htmlFor="date">Data de Nascimento</label>
                      <input type="date" id="nascimento" name="nascimento" value={props.values.nascimento} onChange={props.handleChange} />
+                     {props.errors.nascimento ? (<div>{props.errors.nascimento}</div>) : null}
                   </div>
                   <button type="submit">Adicionar</button>
                </form>
