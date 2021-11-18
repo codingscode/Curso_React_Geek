3140608ae04179f738c337cd4c09c75e8a65ec87
codingscode <heronnestg@hotmail.com>
2021-11-17 22:14:38 -0300

    parte 92

--- 006/react-formulario/src/components/AdicionaCliente.jsx
+++ 006/react-formulario/src/components/AdicionaCliente.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Formik, Field, useField } from 'formik'
+import * as yup from 'yup'
 
 
 
@@ -21,25 +22,23 @@ const Campo = ({ label, ...props}) => {
 
 
 const AdicionaCliente = () => {
+   const esquema = yup.object({
+      nome: yup.string()
+         .required('O nome é obrigatório')
+         .min(10, 'O nome deve ter no mínimo 10 caracteres')
+         .max(30, 'O nome deve ter no máximo 30 caracteres'),
+      email: yup.string().required('O email é obrigatório'),
+      nascimento: yup.date().required('A data de nascimento é obrigatória')
+   })
+
+
    return (
       <>
          <h1>Cadastro de Clientes</h1>
    
-         <Formik initialValues={{ nome: '', email: '', nascimento: '' }} validate={(values) => {
-            const errors = {}
-            if (!values.nome) {
-               errors.nome = 'O nome é obrigatório'
-            }
-            if (!values.email) {
-               errors.email = 'O email é obrigatório'
-            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-               errors.email = 'O email é inválido'               
-            }
-            if (!values.nascimento) {
-               errors.nascimento = 'A data de nascimento é obrigatória'
-            }
-            return errors
-         }} onSubmit={(values) => {
+         <Formik initialValues={{ nome: '', email: '', nascimento: '' }} 
+            validationSchema={esquema}
+            onSubmit={(values) => {
             alert(JSON.stringify(values))
          }} >
             {(props) => (
